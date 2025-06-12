940aa60137c5f55593ed7539a847e9662f191bc0
Zain Shaikh <szain2024@gmail.com>
2025-06-12 14:20:10 +0530

    Tailwind update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,18 +5,18 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <>
-      <div className="header w-full h-20 bg-[#F7F7F7] px-8">
-        <div className="headerLeft">
+      <div className="header w-full h-20 bg-[#e13939] px-8">
+        {/* <div className="headerLeft">
           <Link to="/">
             <img src={logoHead} alt="Logo"  className="h-20"/>
           </Link>
         </div>
         <div className="headerRight">
-          <div><Link to="/"></Link>Home</div>
+          <div className=""><Link to="/"></Link>Home</div>
           <div><Link to="/findmentor">Find A Mentor</Link></div>
           <div><Link to="/apply">Be A Mentor</Link></div>
           <div><Link to="/contact">Contact Us</Link></div>
-        </div>
+        </div> */}
       </div>
     </>
   );
