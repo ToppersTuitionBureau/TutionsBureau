12ae150ef26b84a5af0c7acfce8af664ce8a7d4f
Zain Shaikh <szain2024@gmail.com>
2025-06-12 14:36:54 +0530

    Fonts Updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <>
-      <div className="header w-full h-20 bg-[#e13939] px-8">
-        {/* <div className="headerLeft">
+      <div className="header w-full h-20 bg-[#F7F7F7] px-8">
+        <div className="headerLeft">
           <Link to="/">
             <img src={logoHead} alt="Logo"  className="h-20"/>
           </Link>
@@ -16,7 +16,7 @@ const Header = () => {
           <div><Link to="/findmentor">Find A Mentor</Link></div>
           <div><Link to="/apply">Be A Mentor</Link></div>
           <div><Link to="/contact">Contact Us</Link></div>
-        </div> */}
+        </div>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        anton: ['Anton', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
