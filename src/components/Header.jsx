f0bd9ad518faf5109b101a84f5dc928d015063e6
Zain Shaikh <szain2024@gmail.com>
2025-06-12 13:38:04 +0530

    Logo updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,25 @@
-
-import React from 'react'
+import React from "react";
+import logoHead from "../assets/logo/Logo 2 cropped.jpg";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      Header Content
-    </div>
-  )
-}
+    <>
+      <div className="header w-full h-20 bg-[#F7F7F7] px-8">
+        <div className="headerLeft">
+          <Link to="/">
+            <img src={logoHead} alt="Logo"  className="h-20"/>
+          </Link>
+        </div>
+        <div className="headerRight">
+          <div><Link to="/"></Link>Home</div>
+          <div><Link to="/findmentor">Find A Mentor</Link></div>
+          <div><Link to="/apply">Be A Mentor</Link></div>
+          <div><Link to="/contact">Contact Us</Link></div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Header
+export default Header;
