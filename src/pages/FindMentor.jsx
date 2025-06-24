9defd4fe683d9bde0badd34bfc0f6771ac33f914
SK-2526 <sadiquejet.sk2526@gmail.com>
2025-06-24 18:53:32 +0530

    Footer Added

--- src/pages/FindMentor.jsx
+++ src/pages/FindMentor.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 function FindMentor() {
   return (
     <div><Header />
-    find a mentor</div>
+    find a mentor
+    <Footer /> 
+    </div>
+
   )
 }
 
