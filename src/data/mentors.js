0dd3bf38d523758d1ad7b6d2ed4ded85ffec1946
ToppersTuitionBureau <infotopperstuitionbureau@gmail.com>
2025-07-20 14:25:31 +0530

    Update mentors.js

--- src/data/mentors.js
+++ src/data/mentors.js
@@ -16,10 +16,10 @@ const mentors = [
     experience: 30,
     languages: ["English", "Hindi"],
     rating: 5,
-    subjects: ["Java", "System Design", "DSA"],
+    subjects: ["Java", "DBMS", "OS"],
     price: 5000,
     qualification: "Bcom, MBA(Fin)",
-    image: "https://res.cloudinary.com/dywebgn1p/image/upload/v1752479039/Zain_Draft_twepec.jpg",
+    image: "https://res.cloudinary.com/dywebgn1p/image/upload/v1750756243/Webinar-bro_gpvrnz.svg",
   },
  
 ];
