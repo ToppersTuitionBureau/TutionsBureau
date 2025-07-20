c4c3b132a730f5df8b3441c84a74082735fff3bb
ToppersTuitionBureau <infotopperstuitionbureau@gmail.com>
2025-07-20 14:22:46 +0530

    Update mentors.js

--- src/data/mentors.js
+++ src/data/mentors.js
@@ -10,6 +10,17 @@ const mentors = [
     qualification: "Bcom, MBA(Fin)",
     image: "https://res.cloudinary.com/dywebgn1p/image/upload/v1752479039/Zain_Draft_twepec.jpg",
   },
+  {
+    name: "Zain Shaikh",
+    location: "Mumbai, India",
+    experience: 30,
+    languages: ["English", "Hindi"],
+    rating: 5,
+    subjects: ["Java", "System Design", "DSA"],
+    price: 5000,
+    qualification: "Bcom, MBA(Fin)",
+    image: "https://res.cloudinary.com/dywebgn1p/image/upload/v1752479039/Zain_Draft_twepec.jpg",
+  },
  
 ];
 
