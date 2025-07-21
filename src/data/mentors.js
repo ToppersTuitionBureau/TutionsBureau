2cebaea288c517668238ee39878cd995c55219d3
ToppersTuitionBureau <infotopperstuitionbureau@gmail.com>
2025-07-21 20:40:45 +0530

    Update mentors.js

--- src/data/mentors.js
+++ src/data/mentors.js
@@ -11,14 +11,14 @@ const mentors = [
     image: "https://res.cloudinary.com/dywebgn1p/image/upload/v1752479039/Zain_Draft_twepec.jpg",
   },
   {
-    name: "Zain Shaikh",
+    name: "Sudhir Bhiku Nawale",
     location: "Mumbai, India",
     experience: 30,
     languages: ["English", "Hindi"],
     rating: 5,
-    subjects: ["Java", "DBMS", "OS"],
-    price: 5000,
-    qualification: "Bcom, MBA(Fin)",
+    subjects: ["Chemistry", "NEET", "JEE"],
+    price: 2500,
+    qualification: "BSc, , MSC, MBA, B.Ed",
     image: "https://res.cloudinary.com/dywebgn1p/image/upload/v1753109225/SUDHIR_SIR_2_at7yyf.jpg",
   },
  
